98ba2e213953a43736ac3b978ca1749674093aa5
arnaud sellenet <arnodmental@gmail.com>
2011-12-16 16:08:03 +0100

    initial rails app generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tokboxtest::Application.config.secret_token = 'f91a1ed9822ff07ccec8ae2fccc4b9a7ac11570ed67d10a236f2863221b347e4e65c32dba47d32129e3c91b6a23c3cd2841ea66e219e73728c3ae82d7353c580'
